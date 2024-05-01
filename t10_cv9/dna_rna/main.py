d6d6c16cafec242dbfe543db54d3d8b21cbb2465
Dominika Papánová <dominika.papanova2@gmail.com>
2024-05-01 22:52:01 +0200

    zadanie 4

--- t10_cv9/dna_rna/main.py
+++ t10_cv9/dna_rna/main.py
@@ -1,7 +1,34 @@
 # TODO Task 1
+import random
+from Bio import SeqIO
 
+# record = Seq10.read("inputs/sequence.gb", "genbank")
+# print(record)
 # TODO Task 2
 
-# TODO Task 3
+# dna = "5'-TACCGGAT"
+# table = dna.maketrans("ATGC", "TACG")
+# print(dna.translate(table))
 
+# TODO Task 3
+record = SeqIO.read("inputs/sequence.fasta", "fasta")
+print(record.seq)
+
+
+def mutate(dna):
+    dna_list = list(dna)
+    index = random.randint(0, len(dna_list)-1)
+    dna_list[index] = random.choice("ATGC")
+    return "".join(dna_list)
+
+
+dna = record.seq
+for i in range(1000):
+    dna = mutate(dna)
+
+print(dna)
 # TODO Task 4
+
+# C+G/DLZKA_DNA
+count_seq = (record.count("C") + record.count("G")) / len(record.seq)
+print(count_seq)
